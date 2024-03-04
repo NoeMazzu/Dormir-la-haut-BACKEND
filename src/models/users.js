ade2f1c6b1425d15cbcc296e149f313c9beeca6b
jfbonglet <jean.francois.bonglet@gmail.com>
2024-03-04 11:30:18 +0100

    MAJ user model

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,65 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+      },
+      lastName: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      mail: {
+        type: String,
+        required: true
+      },
+      fav_POI: [{
+        type: Schema.Types.ObjectId,
+        ref: 'POIS'
+      }],
+      fav_meteo: [{
+        type: String
+      }],
+      checklists: [checklist_user],
+      userName: {
+        type: String,
+        required: true
+      },
+      myLocation: {
+        longitude: {
+          type: Number
+        },
+        latitude: {
+          type: Number
+        }
+      }
+});
+
+const checklist_user = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    tasks:[task]
+  });
+
+const task = new Schema({
+itemName: {
+    type: String,
+    required: true
+},
+complete: {
+    type: Boolean
+}
+});  
 
 const User = mongoose.model("users", userSchema);
 
