6d041d7bbad6c1186b322c92464a15b77c9d05bb
Noé <n.mazzu@hotmail.fr>
2024-03-04 12:17:40 +0100

    model users

--- src/models/users.js
+++ src/models/users.js
@@ -1,65 +1,111 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-      },
-      lastName: {
-        type: String,
-        required: true
-      },
-      token: {
-        type: String,
-        required: true
-      },
-      password: {
-        type: String,
-        required: true
-      },
-      mail: {
-        type: String,
-        required: true
-      },
-      fav_POI: [{
-        type: Schema.Types.ObjectId,
-        ref: 'POIS'
-      }],
-      fav_meteo: [{
-        type: String
-      }],
-      checklists: [checklist_user],
-      userName: {
-        type: String,
-        required: true
-      },
-      myLocation: {
-        longitude: {
-          type: Number
-        },
-        latitude: {
-          type: Number
-        }
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isAlpha(value)) {
+        throw new Error(
+          "Invalid Firstname - Firstname does not only contain letters"
+        );
       }
+    },
+  },
+  lastName: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isAlpha(value)) {
+        throw new Error(
+          "Invalid Lastname - Lastname does not only contain letters"
+        );
+      }
+    },
+  },
+  token: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 32, // TODO: Valider la longueur du token
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 9,
+    validate(value) {
+      if (
+        !value.match(/\d/) ||
+        !value.match(/[a-zA-Z]/) ||
+        !value.match(/[!@#$%&]/)
+      ) {
+        throw new Error(
+          "Password must contain at least one letter, one number, and one special character"
+        );
+      }
+    },
+  },
+  mail: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  fav_POI: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "POIS",
+    },
+  ],
+  fav_meteo: [
+    {
+      type: String,
+    },
+  ],
+  checklists: [checklist_user],
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    maxLength: 20,
+  },
+  myLocation: {
+    longitude: {
+      type: Number,
+    },
+    latitude: {
+      type: Number,
+    },
+  },
 });
 
 const checklist_user = new Schema({
-    title: {
-      type: String,
-      required: true
-    },
-    tasks:[task]
-  });
+  title: {
+    type: String,
+    required: true,
+    maxLength: 20,
+  },
+  tasks: [task],
+});
 
 const task = new Schema({
-itemName: {
+  itemName: {
     type: String,
-    required: true
-},
-complete: {
-    type: Boolean
-}
-});  
+    required: true,
+  },
+  complete: {
+    type: Boolean,
+  },
+});
 
 const User = mongoose.model("users", userSchema);
 
