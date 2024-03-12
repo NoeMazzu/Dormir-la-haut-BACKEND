c4acd5fbb9f8b5aa5ac50e373ae9699bc2de24a9
Charles Bréton <charlesbreton@outlook.com>
2024-03-12 22:32:39 +0100

    testdirect

--- src/routes/cloudinary.js
+++ src/routes/cloudinary.js
@@ -4,6 +4,7 @@ const uniqid = require("uniqid");
 const fs = require("fs");
 const cloudinary = require("cloudinary").v2;
 
+//! ROUTE AVEC TEMP
 router.post("/upload-image", async (req, res) => {
   // console.log(req.files);
   const image_file = req.files.photoNewPoi;
@@ -37,6 +38,7 @@ router.post("/upload-image", async (req, res) => {
   });
 });
 
+//! ROUTE DU COURS LA CAPSULE
 router.post("/upload", async (req, res) => {
   const photoPath = `./src/temp/${uniqid()}.png`;
   const resultMove = await req.files.photoNewPoi.mv(photoPath);
@@ -48,6 +50,47 @@ router.post("/upload", async (req, res) => {
   } else {
     return res.json({ result: false, error: resultMove });
   }
+});
+
+//! ROUTE DIRECTE
+router.post("/upload-image-directly", async (req, res) => {
+try {
+  const image_file = req.files.photoNewPoi;
+
+  // Ensure that a file is uploaded
+  if (!image_file) {
+    return res.status(400).json({
+      success: false,
+      message: "No file uploaded",
+    });
+  }
+
+  const upload_res = await cloudinary.uploader.upload(image_file.data, {
+    resource_type: "auto", // Automatically detect the resource type
+    public_id: `DLH/poi-${uniqid()}`,
+    overwrite: false,
+    file: { content: image_file.data }
+  });
+
+  if (upload_res) {
+    return res.status(201).json({
+      success: true,
+      response: upload_res,
+      cdn_url: upload_res.secure_url,
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to upload image to Cloudinary",
+    });
+  }
+} catch (error) {
+  console.error("Error:", error);
+  return res.status(500).json({
+    success: false,
+    message: "An error occurred while processing the request",
+  });
+}
 });
 
 module.exports = router;
