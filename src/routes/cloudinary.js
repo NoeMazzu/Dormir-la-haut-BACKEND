92ee6d2ff70f46ed8041298616531ad9ad018e03
Charles Bréton <charlesbreton@outlook.com>
2024-03-12 15:36:40 +0100

    routeKIMarchPo

--- src/routes/cloudinary.js
+++ src/routes/cloudinary.js
@@ -2,20 +2,21 @@ const express = require("express");
 const router = express.Router();
 const uniqid = require("uniqid");
 const fs = require("fs");
-
 const cloudinary = require("cloudinary").v2;
 
 router.post("/upload-image", async (req, res) => {
-  // const token = req.body.token;
+  // console.log(req.files);
   try {
     const image_file = req.files.photoNewPoi;
-    const temp_image = `/temp/images/${uniqid()}.jpg`;
+    const temp_image = `./temp/${uniqid()}.jpg`;
+    console.log('TEMP Image :',temp_image);
     const move_result = await image_file.mv(temp_image);
+    console.log('moveResult', move_result)
 
     if (move_result === undefined) {
       const upload_res = await cloudinary.uploader.upload(temp_image, {
         resource_type: "image",
-        public_id: `images/users-profiles/user-${uniqid()}`,
+        public_id: `DLH/poi-${uniqid()}`,
         overwrite: false, //! overwrites the existing picture at specified public_id
       });
 
@@ -43,6 +44,20 @@ router.post("/upload-image", async (req, res) => {
     response: "route default response",
     message: "shit happened outside of the try/catch block",
   });
+  // res.json({ body: req.files })
 });
+
+router.post('/upload', async (req, res) => {
+  const photoPath = `./temp/${uniqid()}.png`;
+  const resultMove = await req.files.photoNewPoi.mv(photoPath);
+ 
+  if (!resultMove) {
+     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+     fs.unlinkSync(photoPath); 
+    return res.json({ result: true, url: resultCloudinary.secure_url});      
+  } else {
+    return res.json({ result: false, error: resultMove });
+  }
+ });
 
 module.exports = router;
