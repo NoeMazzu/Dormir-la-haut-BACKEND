ee7f3c02bafd6afbe42776dad289831fa1480e24
Charles Bréton <charlesbreton@outlook.com>
2024-03-13 11:57:36 +0100

    testSansUrl

--- src/models/pois.js
+++ src/models/pois.js
@@ -9,7 +9,7 @@ const validator = require("validator");
 const photos = new Schema({
   url: {
     type: String,
-    required: true,
+    // required: true,
     validate(value) {
       if (!validator.isURL(value)) {
         throw new Error("Invalid URL");
