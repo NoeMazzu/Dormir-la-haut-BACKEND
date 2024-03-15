a8769ee44b49db13f85fada010b666fcc06f717b
jfbonglet <jean.francois.bonglet@gmail.com>
2024-03-15 17:35:30 +0100

    MAJ back sur decoupage de la route NewsAPI

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,26 +3,27 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 const today = new Date();
 const sevenDaysAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
-const formattedToday = today.toISOString().split('T')[0];
-const formattedSevenDaysAgo = sevenDaysAgo.toISOString().split('T')[0];
+const formattedToday = today.toISOString().split("T")[0];
+const formattedSevenDaysAgo = sevenDaysAgo.toISOString().split("T")[0];
 
-const searchQuery = 'alpes AND (meteo OR météo)';
+const searchQuery = "alpes AND (meteo OR météo)";
 const encodedSearchQuery = encodeURIComponent(searchQuery);
 
-const queryAPI = async req => {
-	const options = {
-		method: 'GET',
-		redirect: 'follow',
-	};
+const queryAPI = async () => {
+  try {
+    const options = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-	const apiUrl = `https://newsapi.org/v2/everything?q=${encodedSearchQuery}&apiKey=${NEWS_API_KEY}&language=fr&from=${formattedSevenDaysAgo}&to=${formattedToday}&searchIn=title`;
-
-	fetch(apiUrl, options)
-		.then(response => response.json())
-		.then(data => {
-			return data;
-		})
-		.catch(error => error);
+    const apiUrl = `https://newsapi.org/v2/everything?q=${encodedSearchQuery}&apiKey=${NEWS_API_KEY}&language=fr&from=${formattedSevenDaysAgo}&to=${formattedToday}&searchIn=title`;
+    const APIresponse = await fetch(apiUrl, options);
+	const data = await APIresponse.json();
+    return data
+	
+  } catch (error) {
+	return error
+  }
 };
 
 module.exports = queryAPI;
